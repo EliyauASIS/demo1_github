5459e44ff5d54e8a3d6ae6340a442355804ccef1
Amirschn2 <56786768+Amirschn2@users.noreply.github.com>
2023-11-05 21:03:08 +0200

    Update Page.js

--- Scripts/Page.js
+++ Scripts/Page.js
@@ -4,15 +4,28 @@ let title = document.getElementById("title2")
 
 title.innerHTML += user.firstname + " " + user.lastname
 
+postArr = "";
 
 
 let xhr = new XMLHttpRequest();
-xhr.open("Get","https://jsonplaceholder.typicode.com/posts", true);
+xhr.open("Get", "https://jsonplaceholder.typicode.com/posts", true);
 xhr.onload = function () {
-  let response = JSON.parse(this.responseText);
-  console.log(response);
-  
+    let response = JSON.parse(this.responseText);
+    console.log(response);
+
 };
 
 xhr.send();
 
+const showPost = () => {
+
+    postArr = response;
+
+    for (let x in postArr) {
+
+        let div = document.createElement("div");
+        div.className = "post";
+        container.appendChild("div");
+    }
+        let container = document.getElementById("container2"); 
+}  
