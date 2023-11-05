671ccd7ed8619b1fa80d2df959e065addff037b3
Amirschn2 <56786768+Amirschn2@users.noreply.github.com>
2023-11-05 20:51:17 +0200

    amir's update2

--- Scripts/Page.js
+++ Scripts/Page.js
@@ -3,3 +3,16 @@ let user = JSON.parse(sessionStorage.getItem("user"))
 let title = document.getElementById("title2")
 
 title.innerHTML += user.firstname + " " + user.lastname
+
+
+
+let xhr = new XMLHttpRequest();
+xhr.open("Get","https://jsonplaceholder.typicode.com/posts", true);
+xhr.onload = function () {
+  let response = JSON.parse(this.responseText);
+  console.log(response);
+  
+};
+
+xhr.send();
+
