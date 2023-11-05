da8ab3d68477d7611060a7a5e896f7e9857623db
EliyauASIS <eliyau987@gmail.com>
2023-11-05 21:08:21 +0200

    Eliyahu's change 4

--- Scripts/Page.js
+++ Scripts/Page.js
@@ -6,26 +6,23 @@ title.innerHTML += user.firstname + " " + user.lastname
 
 postArr = "";
 
-
 let xhr = new XMLHttpRequest();
 xhr.open("Get", "https://jsonplaceholder.typicode.com/posts", true);
 xhr.onload = function () {
     let response = JSON.parse(this.responseText);
     console.log(response);
-
+    postArr = response;
 };
-
 xhr.send();
 
+let container = document.getElementById("container2"); 
+
 const showPost = () => {
-
-    postArr = response;
-
     for (let x in postArr) {
-
         let div = document.createElement("div");
         div.className = "post";
         container.appendChild("div");
-    }
-        let container = document.getElementById("container2"); 
+        let post = postArr[x]
+        div.innerHTML = `<ul></ul>`
+    } 
 }  
